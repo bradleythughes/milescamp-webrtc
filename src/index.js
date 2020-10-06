8233d39274f551ceb4fdeae3285f048933e6a48b
Bradley T. Hughes <bradley.hughes@miles.no>
2020-10-06 23:40:22 +0200

    Move the App component to its own directory
    All components will be placed in src/components/ComponentName.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import "webrtc-adapter";
-import App from "./App";
 import { store } from "./store";
+import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(
