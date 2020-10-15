33e805cf721c049382df08fa7aa6ecaa1f6150ad
Bradley T. Hughes <bradley.hughes@miles.no>
2020-10-15 07:26:13 +0200

    Show local camera stream in UserMedia
    Call getUserMedia() from the UserMedia saga and dispatch an action
    containing the captured stream. The reducer puts the stream in the
    store, which gets picked up by the UserMedia useEffect() hook and sets
    the <video> srcObject.

--- src/components/UserMedia/saga.js
+++ src/components/UserMedia/saga.js
@@ -1,1 +1,11 @@
-export default function* () {}
+import { put } from "redux-saga/effects";
+import { userMediaStreamChanged } from "./actions";
+
+export default function* () {
+  try {
+    const stream = yield navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+    yield put(userMediaStreamChanged(stream));
+  } catch (error) {
+    console.error("Failed to get user media", { error });
+  }
+}
