c5f853734abf676c0cbd38ac6b4047837760b58c
Bradley T. Hughes <bradley.hughes@miles.no>
2020-10-21 13:46:41 +0200

    Change getUserMedia() constraints to capture video at 720p

--- src/components/UserMedia/saga.js
+++ src/components/UserMedia/saga.js
@@ -3,7 +3,13 @@ import { userMediaStreamChanged } from "./actions";
 
 export default function* () {
   try {
-    const stream = yield navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+    const stream = yield navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: {
+        width: { ideal: 1280 },
+        height: { ideal: 720 },
+      },
+    });
     yield put(userMediaStreamChanged(stream));
   } catch (error) {
     console.error("Failed to get user media", { error });
