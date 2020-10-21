276d03733e298975745cc50216ced082b8af758f
Bradley T. Hughes <bradley.hughes@miles.no>
2020-10-21 12:21:48 +0200

    Add signaling to start connecting to remote client
    Send a "start" message to the remote client when starting the
    RemoteMedia saga. Likewise, when receiving "start" from the remote
    client, we create an RTCPeerConnection, add the camera and microphone
    tracks from UserMedia, and create an eventChannel that we can use to
    feed events from the peer connection into Redux. The peer connection
    should be closed when the remote client disconnects via its channel,
    since RTCPeerConnection does not have a "close" event of its own.

--- src/components/RemoteMedia/reducer.js
+++ src/components/RemoteMedia/reducer.js
@@ -1,13 +1,27 @@
 import produce from "immer";
-import { REMOTE_MEDIA_STREAM_CHANGED } from "./constants";
+import {
+  PEER_CONNECTION_ON_OPEN,
+  PEER_CONNECTION_ON_CLOSE,
+  REMOTE_MEDIA_STREAM_CHANGED,
+} from "./constants";
 
 export const initialState = {
+  peerConnection: null,
+  peerConnectionChannel: null,
   stream: null,
 };
 
 export default (state = initialState, action) =>
   produce(state, (draft) => {
     switch (action.type) {
+      case PEER_CONNECTION_ON_OPEN:
+        draft.peerConnection = action.peerConnection;
+        draft.peerConnectionChannel = action.peerConnectionChannel;
+        break;
+      case PEER_CONNECTION_ON_CLOSE:
+        draft.peerConnection = null;
+        draft.peerConnectionChannel = null;
+        break;
       case REMOTE_MEDIA_STREAM_CHANGED:
         draft.stream = action.stream;
         break;
--- src/components/RemoteMedia/saga.js
+++ src/components/RemoteMedia/saga.js
@@ -1,1 +1,49 @@
-export default function* () {}
+import { eventChannel } from "redux-saga";
+import { all, call, cancel, fork, put, select, take, takeEvery } from "redux-saga/effects";
+import { peerConnectionOnClose, peerConnectionOnOpen } from "./actions";
+import { PEER_CONNECTION_ON_CLOSE } from "./constants";
+import { createRemoteMediaSelector } from "./selectors";
+import { webSocketSendMessage } from "../App/actions";
+import { WEB_SOCKET_ON_OPEN, WEB_SOCKET_ON_MESSAGE } from "../App/constants";
+import { USER_MEDIA_STREAM_CHANGED } from "../UserMedia/constants";
+import { createUserMediaSelector } from "../UserMedia/selectors";
+
+function* startPeerConnectionSaga() {
+  const peerConnection = new RTCPeerConnection();
+
+  const userMedia = yield select(createUserMediaSelector());
+  userMedia.stream.getTracks().forEach((track) => peerConnection.addTrack(track, userMedia.stream));
+
+  const peerConnectionChannel = yield call(eventChannel, (emit) => {
+    return () => {
+      peerConnection.close();
+      emit(peerConnectionOnClose());
+    };
+  });
+
+  yield takeEvery(peerConnectionChannel, function* (action) {
+    yield put({ ...action });
+  });
+
+  yield put(peerConnectionOnOpen(peerConnection, peerConnectionChannel));
+
+  yield take(PEER_CONNECTION_ON_CLOSE);
+  yield cancel();
+}
+
+function* closePeerConnection() {
+  const { peerConnectionChannel } = yield select(createRemoteMediaSelector());
+  peerConnectionChannel.close();
+}
+
+export default function* () {
+  yield all([take(WEB_SOCKET_ON_OPEN), take(USER_MEDIA_STREAM_CHANGED)]);
+
+  yield takeEvery(`${WEB_SOCKET_ON_MESSAGE}/start`, function* () {
+    yield fork(startPeerConnectionSaga);
+  });
+
+  yield takeEvery(`${WEB_SOCKET_ON_MESSAGE}/close`, closePeerConnection);
+
+  yield put(webSocketSendMessage({ type: "start" }));
+}
